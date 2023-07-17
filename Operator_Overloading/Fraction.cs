b795df3eb516442e1d7b18d64e24f2d58e915bf3
moshesilver <moshetzvi03@gmail.com>
2023-07-17 14:29:59 -0400

    Add project files.

---
+++ Operator_Overloading/Fraction.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator_Overloading
+{
+    public class Fraction
+    {
+        //FIELDS
+        private double numerator;
+        private double denominator;
+
+        //CONSTRUCTORS
+        public Fraction(double numerator, double denominator)
+        {
+            this.Numerator = numerator;
+            this.Denominator = denominator;
+        }
+        
+        //OPERATOR OVERLOADS
+        public static Fraction operator+ (Fraction a, Fraction b)
+        {
+            double nSum = a.numerator + b.numerator;
+            double dSum = a.denominator + b.denominator;
+            return new Fraction(nSum, dSum);
+        }
+
+        public static Fraction operator- (Fraction a, Fraction b)
+        {
+            double nDif = a.numerator - b.numerator;
+            double dDif = a.denominator - b.denominator;
+            return new Fraction(nDif, dDif);
+        }
+
+        public static Fraction operator* (Fraction a, Fraction b)
+        {
+            double nProd = a.numerator * b.numerator;
+            double dProd = a.denominator * b.denominator;
+            return new Fraction(nProd, dProd);
+        }
+
+        public static Fraction operator/ (Fraction a, Fraction b)
+        {
+            double nQuot = a.numerator / b.numerator;
+            double dQuot = a.denominator / b.denominator;
+            return new Fraction(nQuot, dQuot);
+        }
+
+        //PROPERTIES
+        public double Numerator
+        {
+            get { return numerator; }
+            set { numerator = value; }
+        }
+        public double Denominator
+        {
+            get { return denominator; }
+            set
+            {
+                if (value != Math.Round(value))
+                {
+                    Console.WriteLine("invalid input");
+                }
+                else 
+                {
+                    Console.WriteLine("haha");
+                    denominator = value; 
+                }
+            }
+        }
+
+        //METHODS
+        public double Simplify(double n, double d, out double o)
+        {
+            if (((n / 2) == Math.Round(n / 2)) && ((d / 2) == Math.Round(d / 2)))
+            {
+                n /= 2;
+                d /= 2;
+            }
+            o = d;
+            return n;
+        }
+
+        public static void Display(Fraction fTotal)
+        {
+            Console.WriteLine($"{fTotal.Numerator}/{fTotal.Denominator}");
+        }
+
+        //DESTRUCTORS
+
+
+    }
+}
---
+++ Operator_Overloading/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator_Overloading
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please choose a numerator for fraction 1\n\t\tdenominator for fraction 1\n\t\tnumerator for fraction 2\n\t\tdenominator for fraction 2");
+            Fraction f1 = new Fraction(Convert.ToDouble(Console.ReadLine()), Convert.ToDouble(Console.ReadLine()));
+            Fraction f2 = new Fraction(Convert.ToDouble(Console.ReadLine()), Convert.ToDouble(Console.ReadLine()));
+
+            Fraction fSum = f1 + f2;
+            Fraction fDif = f1 - f2;
+            Fraction fProd = f1 * f2;
+            Fraction fQuot = f1 / f2;
+
+            Console.Write("Sum: ");
+            Fraction.Display(fSum);
+            Console.Write("Difference: ");
+            Fraction.Display(fDif);
+            Console.Write("Product: ");
+            Fraction.Display(fProd);
+            Console.Write("Quotient: ");
+            Fraction.Display(fQuot);
+        }
+    }
+}
+    
+           
